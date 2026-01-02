8b27a507dc4d5af106ced1c58acc1542a76f2c55
Héctor Masip Ardevol <68732820+hecmas@users.noreply.github.com>
2026-01-02 14:40:50 +0000

    Adapating to new sha

--- zisk-programs/sha256/program/src/main.rs
+++ zisk-programs/sha256/program/src/main.rs
@@ -6,6 +6,7 @@ use ziskos::{
     syscalls::{syscall_sha256_f, SyscallSha256Params},
 };
 
+#[allow(deprecated)]
 use generic_array::{typenum::U64, GenericArray};
 use rand::Rng;
 use sha2::compress256;
@@ -20,7 +21,13 @@ fn main() {
 
     let num_sha256fs =
         usize::from_le_bytes(input[..8].try_into().expect("Input should be at least 8 bytes"));
-    println!("Number of sha256f to compute: {:?}", num_sha256fs);
+
+    if ACTIVATE_CONSISTENCY_TEST {
+        println!("Running SHA256F consistency test for {} times", num_sha256fs);
+    } else {
+        println!("Running SHA256F random tests for {} times", num_sha256fs);
+    }
+
     for _ in 0..num_sha256fs {
         if ACTIVATE_CONSISTENCY_TEST {
             run_consistency_test();
@@ -30,27 +37,29 @@ fn main() {
     }
 }
 
+// Take any number and apply the sha256f function
+#[allow(deprecated)]
 fn sha256f_apply(rng: &mut rand::rngs::ThreadRng) {
-    // Take any number and apply the sha256f function
     let mut state_u32 = [0u32; 8];
     for i in 0..8 {
         state_u32[i] = rng.gen();
     }
 
-    let mut input = [0u64; 8];
-    for i in 0..8 {
-        input[i] = rng.gen();
+    let mut input_u8 = [0u8; 64];
+    for i in 0..64 {
+        input_u8[i] = rng.gen();
     }
 
     let mut state_u32_copy = state_u32.clone();
 
-    let mut state: &mut [u64; 4] = unsafe { &mut *(state_u32.as_mut_ptr() as *mut [u64; 4]) };
-    let mut params = SyscallSha256Params { state: &mut state, input: &input };
+    let state: &mut [u64; 4] = unsafe { &mut *(state_u32.as_mut_ptr() as *mut [u64; 4]) };
+    let input: &[u64; 8] = unsafe { &*(input_u8.as_ptr() as *const [u64; 8]) };
+    let mut params = SyscallSha256Params { state, input };
     syscall_sha256_f(&mut params);
 
     // Compare against an audited sha256f implementation
-    let input_u8: GenericArray<u8, U64> = convert_u64_to_generic_array_bytes(&input);
-    compress256(&mut state_u32_copy, &[input_u8]);
+    let input_ga: GenericArray<u8, U64> = GenericArray::clone_from_slice(&input_u8);
+    compress256(&mut state_u32_copy, &[input_ga]);
     let expected_result: &[u64; 4] = unsafe { &*(state_u32_copy.as_ptr() as *const [u64; 4]) };
 
     assert!(
@@ -62,17 +71,18 @@ fn sha256f_apply(rng: &mut rand::rngs::ThreadRng) {
 }
 
 fn run_consistency_test() {
-    println!("Running consistency test for SHA256F...");
-
-    const SHA256_INITIAL_HASH_STATE: [u32; 8] = [
+    const SHA256F_INITIAL_HASH_STATE: [u32; 8] = [
         0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab,
         0x5be0cd19,
     ];
+    const SHA256F_INPUT: [u8; 64] = {
+        let mut a = [0u8; 64];
+        a[0] = 0x80;
+        a
+    };
 
-    let mut state: [u64; 4] = unsafe { *(SHA256_INITIAL_HASH_STATE.as_ptr() as *const [u64; 4]) };
-
-    let mut input = [0u64; 8];
-    input[0] = 1 << 63;
+    let mut state: [u64; 4] = unsafe { *(SHA256F_INITIAL_HASH_STATE.as_ptr() as *const [u64; 4]) };
+    let input: [u64; 8] = unsafe { *(SHA256F_INPUT.as_ptr() as *const [u64; 8]) };
 
     let mut params = SyscallSha256Params { state: &mut state, input: &input };
     syscall_sha256_f(&mut params);
@@ -89,14 +99,4 @@ fn run_consistency_test() {
         expected_result,
         state
     );
-}
-
-fn convert_u64_to_generic_array_bytes(input: &[u64; 8]) -> GenericArray<u8, U64> {
-    let mut out = [0u8; 64];
-    for (i, word) in input.iter().enumerate() {
-        for j in 0..8 {
-            out[i * 8 + j] = (word >> (56 - j * 8)) as u8;
-        }
-    }
-    GenericArray::<u8, U64>::clone_from_slice(&out)
 }
