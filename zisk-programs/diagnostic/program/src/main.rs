159b4940ddebb618edca4ffd62cb522d9d4802f0
Héctor Masip Ardevol <68732820+hecmas@users.noreply.github.com>
2026-02-25 08:40:18 +0000

    Diagnostic fully fixed

--- zisk-programs/diagnostic/program/src/main.rs
+++ zisk-programs/diagnostic/program/src/main.rs
@@ -5,12 +5,12 @@ ziskos::entrypoint!(main);
 mod arith_eq;
 mod arith_eq_384;
 mod bigint;
-// mod fcall;
+mod fcall;
 mod keccakf;
 mod riscv_c;
 mod riscv_fd;
 mod riscv_ima;
-// mod sha256f;
+mod sha256f;
 
 #[cfg(not(all(target_os = "zkvm", target_vendor = "zisk")))]
 fn main() {}
@@ -21,11 +21,11 @@ fn main() {
     riscv_ima::diagnostic_riscv_ima();
     riscv_c::diagnostic_riscv_c();
     riscv_fd::diagnostic_riscv_fd();
-    // fcall::diagnostic_fcall(); //TODO: Fix rom-setup issue
+    fcall::diagnostic_fcall();
     arith_eq::test_arith_eq();
     arith_eq_384::test_arith_eq_384();
     bigint::test_bigint();
     keccakf::test_keccakf();
-    // sha256f::test_sha256f(); //TODO: Fix rom-setup issue
+    sha256f::test_sha256f();
     println!("Success");
 }
